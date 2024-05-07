3d374a80de5d6d4bcdbb4c6467d8d8748016ebe5
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-05-07 19:21:08 +0900

    プロジェクトinit

---
+++ target/debug/deps/rust_lesson-89be077dddf7956f.d
@@ -0,0 +1,5 @@
+/Users/01051530/Desktop/training/rust/rust_lesson/target/debug/deps/librust_lesson-89be077dddf7956f.rmeta: src/main.rs
+
+/Users/01051530/Desktop/training/rust/rust_lesson/target/debug/deps/rust_lesson-89be077dddf7956f.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/rust_lesson.d
@@ -0,0 +1,1 @@
+/Users/01051530/Desktop/training/rust/rust_lesson/target/debug/rust_lesson: /Users/01051530/Desktop/training/rust/rust_lesson/src/main.rs
