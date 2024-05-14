6958a8b0a5d1278779dc9a704dc08c27f7de131c
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-05-14 20:00:34 +0900

    フォーマッター導入

--- src/modules/sec_3.rs
+++ src/modules/sec_3.rs
@@ -1,4 +1,4 @@
-const A: i32 = 100;  // 定数は型推論されない
+const A: i32 = 100; // 定数は型推論されない
 
 // 関数
 fn say_hello() {
@@ -6,10 +6,10 @@ fn say_hello() {
     // 返り値がない場合はunit型が返されている
 }
 
-fn add(a: i32, b:i32) -> i32 {
+fn add(a: i32, b: i32) -> i32 {
     // return a + b;
     a + b // 関数の最後に返したい値をセミコロンなしで書く方が一般的
-    // returnは条件分岐で早期リターンするときに使う
+          // returnは条件分岐で早期リターンするときに使う
 }
 
 fn print_typename<T>(_: T) {
@@ -22,18 +22,18 @@ pub fn sub() {
     println!("Hello, world!");
     // print!("Hello, ");
     // print!("Rust!");
-    
+
     // プレースホルダー{}
     println!("Hello, {}", "students");
-    
+
     println!("------変数、定数------");
-    let a: i32 = 1;  // Rustでは変数に値を入れることを束縛・バインドという
+    let a: i32 = 1; // Rustでは変数に値を入れることを束縛・バインドという
     println!("変数 a = {}", a);
-    
+
     let mut b: i32 = 2;
     println!("変数 b = {}", b);
     b = 3;
-    
+
     println!("変数 b = {}", b);
     println!("定数 A = {}", A);
 
@@ -45,8 +45,8 @@ pub fn sub() {
 
     // 型推論
     println!("------数値型、論理型------");
-    let a10 = 1;    // デフォルトはi32
-    let b10 = 2.0;  // デフォルトはf64
+    let a10 = 1; // デフォルトはi32
+    let b10 = 2.0; // デフォルトはf64
     println!("変数 a10 = {}", a10);
     print_typename(a10);
     println!("変数 b10 = {}", b10);
@@ -61,7 +61,7 @@ pub fn sub() {
     print_typename(d10);
 
     // i32の1をf64にキャスト
-    let f10 :f64 = 1 as f64 + 2.0;
+    let f10: f64 = 1 as f64 + 2.0;
     println!("変数 f10 = {}", f10);
     print_typename(f10);
 
@@ -80,20 +80,20 @@ pub fn sub() {
     let i1 = t1.0;
     println!("変数 i1 = {}", i1);
 
-    let (x,y,z) = t2;
+    let (x, y, z) = t2;
     println!("変数 x = {}", x);
     println!("変数 y = {}", y);
     println!("変数 z = {}", z);
 
     // 参照外し
-    let mut a = 10;            // mutable object
-    let a_mut_ref = &mut a;    // mutable reference
-    *a_mut_ref = 20;           // dereference and assign
+    let mut a = 10; // mutable object
+    let a_mut_ref = &mut a; // mutable reference
+    *a_mut_ref = 20; // dereference and assign
     println!("{}", a_mut_ref); // auto dereference
 
     println!("------配列------");
     // 全て同じ型
-    let l1 = [1,2,3];
+    let l1 = [1, 2, 3];
     println!("変数 l1 = {:?}", l1);
     let l2 = [0; 100];
     println!("変数 l2 = {:?}", l2);
@@ -108,7 +108,7 @@ pub fn sub() {
 
     println!("------ベクタ型------");
     // 配列に似ているが、初期化後に要素数を変更することができる
-    let mut v1 = vec![1,2,3];
+    let mut v1 = vec![1, 2, 3];
     v1.push(1);
     println!("変数 v1 = {:?}", v1);
     let mut v2: Vec<i32> = Vec::new();
@@ -138,12 +138,12 @@ pub fn sub() {
     println!("変数 str2 = {}", str2);
 
     // formatは結合の順番を気にしなくてよい
-    let str3:String = format!("{}{}", str1, str2);
+    let str3: String = format!("{}{}", str1, str2);
     println!("変数 str3 = {}", str3);
 
     // 関数
     say_hello();
-    let ret = add(1,2);
+    let ret = add(1, 2);
     println!("変数 ret = {}", ret);
 
     // 式
@@ -180,10 +180,10 @@ pub fn sub() {
     }
     println!("{}", y);
 
-    
     println!("------if------");
     let x: i32 = 30;
-    if x > 0 && x <= 10 { // 条件部分は必ず論理型でなければならない
+    if x > 0 && x <= 10 {
+        // 条件部分は必ず論理型でなければならない
         println!("{}", "first");
     } else if x > 11 && x <= 20 {
         println!("{}", "second");
@@ -193,6 +193,7 @@ pub fn sub() {
 
     // ifは式なので値を返すことができる
     // 戻り値の型はそろえる
+    #[rustfmt::skip]
     let x2: i32 = if x > 10 {
         x
     } else {
