afb3b8dcd85abf1658723adbcc185c0138a79ebf
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-06-22 13:38:30 +0900

    更新

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ mod modules;
 // use modules::sec_4;
 // use modules::sec_5;
 // use modules::sec_6;
-use modules::sec_7;
+// use modules::sec_7;
+use modules::sec_8;
 
 fn main() {
     // sec_3::sub();
     // sec_4::sub();
     // sec_5::sub();
-    sec_7::sub();
+    // sec_7::sub();
+    sec_8::sub();
 }
