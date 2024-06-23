26e20779f3e27de0188f7a5435f0ae3d7b0031e3
gravity <hideaki.murakami@nijibox.co.jp>
2024-06-23 23:48:50 +0900

    更新

--- src/modules/sec_8.rs
+++ src/modules/sec_8.rs
@@ -1,5 +1,10 @@
+use rand::Rng;
+
 pub fn sub() {
     println!("======ST sec_8======");
+    println!("クレート");
+    let random_number = rand::thread_rng().gen_range(1..101);
+    println!("{}", random_number);
 
     println!("======ED sec_8======");
 }
