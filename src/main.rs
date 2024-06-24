3df2d875eada9dc901e278c5328fdd74a5ac077e
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-06-24 17:31:42 +0900

    更新

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,10 @@
 mod modules;
-// use modules::sec_3;
-// use modules::sec_4;
-// use modules::sec_5;
-// use modules::sec_6;
-// use modules::sec_7;
-use modules::sec_8;
+use modules::*;
 
 fn main() {
     // sec_3::sub();
     // sec_4::sub();
     // sec_5::sub();
-    // sec_7::sub();
+    sec_7::sub();
     sec_8::sub();
 }
--- src/modules/sec_8.rs
+++ src/modules/sec_8.rs
@@ -1,10 +1,36 @@
 use rand::Rng;
 
+// 外部からも呼び出せる
+pub fn test_fn1() {
+    println!("sub1 test_fn1");
+    test_fn2();
+}
+fn test_fn2() {
+    println!("sub1 test_fn2");
+}
+
+pub struct TestStruct {
+    pub val1: i32,
+    pub val2: i32,
+}
+
 pub fn sub() {
     println!("======ST sec_8======");
     println!("クレート");
     let random_number = rand::thread_rng().gen_range(1..101);
     println!("{}", random_number);
+
+    println!("バイナリとライブラリ");
+    // 今まで書いてきたのはバイナリクレート
+    // 単体で実行可能
+    // cargoはデフォルトでsrc/main.rsをパッケージと同じ名前のバイナリクレートと認識する
+    // main.rs = rust_lessonのバイナリクレートと認識されている
+    // src/bin配下にファイルを配置することで複数のバイナリクレートを持つことができる
+
+    // ライブラリクレートはパッケージで0個または1個もつことができる
+    // ライブラリクレートは他のプログラムで取り込まれ機能提供するクレート
+    // バイナリクレート間は機能を共有することができない
+    rust_lesson::say_hello();
 
     println!("======ED sec_8======");
 }
