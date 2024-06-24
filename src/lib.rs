ff03feee953e7187603d31aec5d55f9e5721fab8
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-06-24 17:46:20 +0900

    更新

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,12 @@
+//! ライブラリクレートのドキュメント
 // 必ずpubにする
+// マークダウンが使える
+/// ### 使用例
+/// ```
+/// fn main() {
+///     rust_lesson::say_hello();
+/// }
+/// ```
 pub fn say_hello() {
     println!("Hello.");
 }
