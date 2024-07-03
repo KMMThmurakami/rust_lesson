888d0ac66fa674a6de0663bfd8309535e22577d8
hideaki.murakami <hideaki.murakami@nijibox.co.jp>
2024-07-03 13:55:40 +0900

    sec15

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 // mod modules;
 // use modules::*;
+use std::io;
 
 fn main() {
-    // test::sub();
+    // 実行内容を取得
+    let mut service_type: String = String::new();
+    println!("実行したい内容を入力してください(0:登録、1:集計)");
+    io::stdin().read_line(&mut service_type).unwrap();
+    let service_type: u8 = service_type.trim().parse().expect("数値で入力してください");
+
+    // 入力値のバリデーション
+
+    if service_type == 0 {
+        println!("登録");
+    } else if service_type == 1 {
+        println!("集計");
+    }
 }
